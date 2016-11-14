8d9c71beb969cfe7328dd1d123a697364c0c1cd9
Ramonauta <lfcarballo@gmail.com>
2016-11-14 19:45:50 -0300

    segundo comit clase 1

---
+++ jsEntradaSalida-5.js
@@ -0,0 +1,12 @@
+/*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
+ej.: "Usted se llama José y tiene 66 años" 	*/
+function Mostar()
+{	
+	var nombre;
+	var edad; 
+	
+	document.getElementById('elNombre').value;
+	document.getElementById('laEdad').value;
+	alert("usted se llama: "+nombre+" y tiene: "+Edad+ "años");
+}
+
